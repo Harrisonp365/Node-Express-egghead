2db4419edabb4eb48f79605c5550639aa23748c0
Harrisonp365 <harrisonpowell365@gmail.com>
2021-12-20 11:08:23 +1100

    lesson 3 and 4, there are extra dependencies and issues with Jade(pug) that need to be worked out that are not in the course

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("hello world!");
+const fs = require("fs");
+const _ = require("lodash");
+const engines = require("consolidate");
+let users = [];
+
+fs.readFile("users.json", { encoding: "utf8" }, function (err, data) {
+  if (err) throw err;
+
+  JSON.parse(data).forEach(function (user) {
+    user.name.full = _.startCase(user.name.first + " " + user.name.last);
+    users.push(user);
+  });
 });
 
-app.get("/yo", function (req, res) {
-  res.send("Yo");
+app.engine("hbs", engines.handlebars);
+app.set("views", "./views");
+app.set("view engine", "hbs");
+
+app.use("/profilepics", express.static("images"));
+
+app.get("/", function (req, res) {
+  res.render("index", { users: users });
+});
+
+//Route placement in express matters, once res.send is called nothing else will be called!!
+app.get("/:username", function (req, res) {
+  let username = req.params.username;
+  res.render("user", { username: username });
 });
 
 const server = app.listen(3000, function () {
